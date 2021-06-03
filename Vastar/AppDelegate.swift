229618ceb4c29d26ac67727fcf708c81cef99ab4
Charles Kuo <>
2021-06-03 14:47:16 +0800

    修改AppDelegate 起始頁問題

--- Vastar/AppDelegate.swift
+++ Vastar/AppDelegate.swift
@@ -15,14 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let rootVC = LoginViewController(nibName: "LoginViewController", bundle: nil)
-        rootVC.modalPresentationStyle = .fullScreen
-        
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = rootVC
-        window?.makeKeyAndVisible()
-        
-        
+        if #available(iOS 13.0, *) {
+            
+        }else {
+            let rootVC = LoginViewController(nibName: "LoginViewController", bundle: nil)
+            rootVC.modalPresentationStyle = .fullScreen
+            
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = rootVC
+            window?.makeKeyAndVisible()
+        }
+
         return true
     }
 
